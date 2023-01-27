b84ae2fe415ab43627cf0d3b1e271fba60f21626
vvkquau <ducnhan141106@gmail.com>
2023-01-28 00:16:17 +0700

    done

---
+++ index.js
@@ -0,0 +1,19 @@
+function func1() {
+    navigator.clipboard.writeText("0932624904");
+    alert("Copied!");
+}
+
+function func2() {
+    navigator.clipboard.writeText("0621000456274");
+    alert("Copied!");
+}
+
+function func3() {
+    navigator.clipboard.writeText("791189037896");
+    alert("Copied!");
+}
+
+function func4() {
+    navigator.clipboard.writeText("060199006098");
+    alert("Copied!");
+}
